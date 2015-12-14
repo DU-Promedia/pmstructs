517a5024875bcd5c4796dd414a48320d4abfa1a7
Kent Cederström <kent@kentcederstrom.se>
2015-12-14 22:22:14 +0100

    import lists

--- articlelist_imports.go
+++ articlelist_imports.go
@@ -2,6 +2,7 @@ package pmstructs
 
 import (
 	"encoding/xml"
+	"log"
 	"net/url"
 
 	"gopkg.in/mgo.v2"
@@ -11,6 +12,11 @@ import (
 /*
  * Sections, or lists of content that's parsed
  */
+
+type ArticleListRootElement struct {
+	XMLName xml.Name
+}
+
 type ArticleListWrapper struct {
 	XMLName xml.Name    `xml:"MobileContent"`
 	TheList ArticleList `xml:"MobileContentBlocks"`
@@ -21,8 +27,8 @@ type ArticleList struct {
 	OriginID    string        `xml:"id,attr" bson:"originid" json:"id"`
 	Origin      string        `bson:"origin" json:"origin"`
 	Url         string        `json:"url" bson:"url"`
-	Articles    []Article     `xml:"MobileContentBlock>StandardArticle" bson:"-" json:"-"`
 	ArticleList []ArticleRef  `bson:"articlelist" json:"articlelist"`
+	Articles    []Article     `xml:"MobileContentBlock>StandardArticle" bson:"-" json:"-"`
 }
 
 type ArticleContentPlacement struct {
@@ -31,8 +37,8 @@ type ArticleContentPlacement struct {
 	OriginID    string        `xml:"id,attr" bson:"originid" json:"id"`
 	Origin      string        `bson:"origin" json:"origin"`
 	Url         string        `json:"url" bson:"url"`
-	Articles    []Article     `xml:"StandardArticle" bson:"-"`
 	ArticleList []ArticleRef  `bson:"articlelist" json:"articlelist"`
+	Articles    []Article     `xml:"StandardArticle" bson:"-"`
 }
 
 type ArticleStatisticsList struct {
@@ -41,8 +47,8 @@ type ArticleStatisticsList struct {
 	OriginID    string        `xml:"id,attr" bson:"originid" json:"id"`
 	Origin      string        `bson:"origin" json:"origin"`
 	Url         string        `json:"url" bson:"url"`
-	Articles    []Article     `xml:"List>ListItem>StandardArticle" bson:"-" json:"-"`
 	ArticleList []ArticleRef  `bson:"articlelist" json:"articlelist"`
+	Articles    []Article     `xml:"List>ListItem>StandardArticle" bson:"-" json:"-"`
 }
 
 /*
@@ -53,17 +59,35 @@ func (list *ArticleContentPlacement) Save(db *mgo.Database) {
 
 	// Index should be unique by originid so we should be safe
 	parseUrl, _ := url.Parse(list.Url)
-
 	list.Origin = parseUrl.Host
+
 	oldArticles := list.Articles
+	oldReflist := list.ArticleList
 
-	if err := sectionCollection.Insert(list); err != nil {
-		// Update
-		sectionCollection.Update(bson.M{"url": list.Url}, list)
+	// Find in DB
+	err := sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
+	if err != nil {
+		// Found no section for articlelist
+		log.Println("Inserting section")
+		sectionCollection.Insert(list)
+		err = sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
+		if err != nil {
+			log.Println("ArticleContentPlacement:", err)
+			return
+		}
 	}
-	sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
 
-	list.Articles = oldArticles
+	if len(oldReflist) > 0 {
+		list.ArticleList = oldReflist
+	}
+
+	if len(oldArticles) > 0 {
+		list.Articles = oldArticles
+	}
+
+	sectionCollection.Update(bson.M{"url": list.Url}, list)
+
+	//sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
 }
 
 func (list *ArticleContentPlacement) SaveToDB(db *mgo.Database) {
@@ -101,6 +125,76 @@ func (list *ArticleContentPlacement) GetArticles() []Article {
 }
 
 /*
+ * Article list
+ */
+func (list *ArticleList) Save(db *mgo.Database) {
+	sectionCollection := db.C("sections")
+
+	// Index should be unique by originid so we should be safe
+	parseUrl, _ := url.Parse(list.Url)
+	list.Origin = parseUrl.Host
+
+	oldArticles := list.Articles
+	oldReflist := list.ArticleList
+
+	// Find in DB
+	err := sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
+	if err != nil {
+		// Found no section for articlelist
+		log.Println("Inserting section")
+		sectionCollection.Insert(list)
+		err = sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
+		if err != nil {
+			log.Println("ArticleContentPlacement:", err)
+			return
+		}
+	}
+
+	if len(oldReflist) > 0 {
+		list.ArticleList = oldReflist
+	}
+
+	if len(oldArticles) > 0 {
+		list.Articles = oldArticles
+	}
+
+	sectionCollection.Update(bson.M{"url": list.Url}, list)
+
+	//sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
+}
+
+func (list *ArticleList) SaveToDB(db *mgo.Database) {
+
+	list.Save(db)
+
+	i := 0
+
+	for _, a := range list.Articles {
+		i++
+
+		a.SaveToDB(db)
+
+		artRef := ArticleRef{}
+		artRef.ArticleID = a.Id
+		list.ArticleList = append(list.ArticleList, artRef)
+
+		a.SaveToDB(db)
+	}
+
+	list.Save(db)
+}
+
+func (list *ArticleList) GetArticles() []Article {
+	listOfArticles := make([]Article, 0)
+
+	for _, a := range list.Articles {
+		listOfArticles = append(listOfArticles, a)
+	}
+
+	return listOfArticles
+}
+
+/*
  * StatisticsList
  */
 func (list *ArticleStatisticsList) Save(db *mgo.Database) {
@@ -109,15 +203,33 @@ func (list *ArticleStatisticsList) Save(db *mgo.Database) {
 	// Index should be unique by originid so we should be safe
 	parseUrl, _ := url.Parse(list.Url)
 	list.Origin = parseUrl.Host
+
 	oldArticles := list.Articles
+	oldReflist := list.ArticleList
+	savedList := ArticleListCommon{}
 
-	if err := sectionCollection.Insert(list); err != nil {
-		// Update
-		sectionCollection.Update(bson.M{"url": list.Url}, list)
+	// Find in DB
+	err := sectionCollection.Find(bson.M{"url": list.Url}).One(&savedList)
+	if err != nil {
+		sectionCollection.Insert(list)
+		err = sectionCollection.Find(bson.M{"url": list.Url}).One(&savedList)
+		if err != nil {
+			log.Println("ArticleContentPlacement:", err)
+			return
+		}
 	}
-	sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
 
-	list.Articles = oldArticles
+	if len(oldReflist) > 0 {
+		list.ArticleList = oldReflist
+	}
+
+	if len(oldArticles) > 0 {
+		list.Articles = oldArticles
+	}
+
+	sectionCollection.Update(bson.M{"url": list.Url}, list)
+
+	//sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
 }
 
 func (list *ArticleStatisticsList) SaveToDB(db *mgo.Database) {
@@ -149,57 +261,6 @@ func (list *ArticleStatisticsList) GetArticles() []Article {
 		target := make([]Article, len(listOfArticles)+1)
 		copy(target, listOfArticles)
 		listOfArticles = append(target, a)
-	}
-
-	return listOfArticles
-}
-
-/*
- * Article list
- */
-func (list *ArticleList) Save(db *mgo.Database) {
-	sectionCollection := db.C("sections")
-
-	// Index should be unique by originid so we should be safe
-	parseUrl, _ := url.Parse(list.Url)
-	list.Origin = parseUrl.Host
-	oldArticles := list.Articles
-
-	if err := sectionCollection.Insert(list); err != nil {
-		// Update
-		sectionCollection.Update(bson.M{"url": list.Url}, list)
-	}
-	sectionCollection.Find(bson.M{"url": list.Url}).One(&list)
-
-	list.Articles = oldArticles
-}
-
-func (list *ArticleList) SaveToDB(db *mgo.Database) {
-
-	list.Save(db)
-
-	i := 0
-
-	for _, a := range list.Articles {
-		i++
-
-		a.SaveToDB(db)
-
-		artRef := ArticleRef{}
-		artRef.ArticleID = a.Id
-		list.ArticleList = append(list.ArticleList, artRef)
-
-		a.SaveToDB(db)
-	}
-
-	list.Save(db)
-}
-
-func (list *ArticleList) GetArticles() []Article {
-	listOfArticles := make([]Article, 0)
-
-	for _, a := range list.Articles {
-		listOfArticles = append(listOfArticles, a)
 	}
 
 	return listOfArticles
